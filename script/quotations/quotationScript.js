64faeef80526d4e34cc6c6955f82b4c2f2d70d0c
rew17p49 <106513736+rew17p49@users.noreply.github.com>
2022-06-23 15:16:38 +0700

    .

--- script/quotations/quotationScript.js
+++ script/quotations/quotationScript.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
                             return  "<div class='text-center'><div class='btn-group'><button class='btn btn-primary p-1 m-2 ' id='btnEditQuotation' data-toggle='modal'  data-target='#modalQuotationEditMaster'  style='width: 2rem;''><i class='fa fa-pencil-square-o'></i></button><button  class='btn btn-danger p-1 m-2' id='btnDelProject' data-toggle='modal' data-target='#modalDeleteConfirm' style='width: 2rem;''><i class='fa fa-remove'></i></button></div></div>"
                             ;}
                             if ( row.StatusName === 'quotation') {
-                                return  "<div class='text-center'><div class='btn-group'><button class='btn btn-warning p-1 m-2 ' id='btnEditQuotation' data-toggle='modal'  data-target='#modalQuotationRevisedMaster'  style='width: 2rem;''><i class='fa fa-file-o'></i></button><button  class='btn btn-danger p-1 m-2 disabled' id='btnDelProject' data-toggle='modal' data-target='#modalDeleteConfirm' style='width: 2rem;''><i class='fa fa-remove'></i></button></div></div>"
+                                return  "<div class='text-center'><div class='btn-group'><button class='btn btn-warning p-1 m-2 ' id='btnRevisedQuotation' data-toggle='modal'  style='width: 2rem;''><i class='fa fa-file-o'></i></button><button  class='btn btn-danger p-1 m-2 disabled' id='btnDelProject' data-toggle='modal' data-target='#modalDeleteConfirm' style='width: 2rem;''><i class='fa fa-remove'></i></button></div></div>"
                                 ;}
                                     if ( row.StatusName === 'cancel') {
                                         return  "<div class='text-center'><div class='btn-group'><button class='btn btn-primary p-1 m-2 disabled' id='btnEditQuotation' data-toggle='modal'  data-target='#modalQuotationEditMaster'  style='width: 2rem;''><i class='fa fa-pencil-square-o'></i></button><button  class='btn btn-danger p-1 m-2 disabled' id='btnDelProject' data-toggle='modal' data-target='#modalDeleteConfirm' style='width: 2rem;''><i class='fa fa-remove'></i></button></div></div>"
@@ -126,7 +126,7 @@ $(document).ready(function () {
                     $('#modalEditProjectName').val(obj.QuotationSubject);
       				$('#modalEditDiscout').val(obj.QuotationDiscount);
                     $('#modalEditValidity').val(obj.QuotationValidityDate);
-                    $('#modalEditPayment').val(obj.QuotationPayterm);
+                    $('#modalEditPayment').val(obj.QuotationPayTerm);
                     $('#modalEditDelivery').val(obj.QuotationDelivery);
                     $('#modalEditRemark').val(obj.QuotationRemark);
                     $('#modalEditApprove').val(obj.EmployeeApproveId);
@@ -139,7 +139,7 @@ $(document).ready(function () {
                 let QuotationSubject = $.trim($('#modalEditProjectName').val());
                 let QuotationDiscount = $.trim($('#modalEditDiscout').val());
                 let QuotationValidityDate = $.trim($('#modalEditValidity').val());
-                let QuotationPayterm = $.trim($('#modalEditPayment').val());
+                let QuotationPayTerm = $.trim($('#modalEditPayment').val());
                 let QuotationDelivery = $.trim($('#modalEditDelivery').val());
                 let QuotationRemark = $.trim($('#modalEditRemark').val());
                 let EmployeeApproveId = $.trim($('#modalEditApprove').val());
@@ -153,7 +153,7 @@ $(document).ready(function () {
                         QuotationSubject: QuotationSubject,
                         QuotationDiscount: QuotationDiscount,
                         QuotationValidityDate: QuotationValidityDate,
-                        QuotationPayterm: QuotationPayterm,
+                        QuotationPayTerm: QuotationPayTerm,
                         QuotationDelivery: QuotationDelivery,
                         QuotationRemark: QuotationRemark,
                         EmployeeApproveId: EmployeeApproveId
@@ -185,6 +185,82 @@ $(document).ready(function () {
                     }
                 });
         })
+    });
+    // Revised
+    $(document).on("click", "#btnRevisedQuotation", function () {
+        $(".modal-title").text("Edit Quotation");
+        // console.log("save0");
+        rows = $(this).closest("tr");
+        let QuotationId = tableQuo.rows(rows).data()[0].QuotationId;
+        $.ajax({
+            url: "/quotation/" + QuotationId,
+            method: 'get',
+            cache: false,
+			success:function(response){
+				// console.log(QuotationId);
+				var obj = JSON.parse(response);
+
+                    let QuotationNoId = obj.QuotationNoId;
+                    let QuotationRevised = obj.QuotationRevised;
+                    let QuotationStatus = obj.QuotationStatus;
+                    let QuotationSubject = obj.QuotationSubject;
+                    let QuotationTotalPrice = obj.QuotationTotalPrice;
+                    let QuotationDiscount = obj.QuotationDiscount;
+                    let QuotationValidityDate = obj.QuotationValidityDate;
+                    let QuotationPayTerm = obj.QuotationPayTerm;
+                    let QuotationDelivery = obj.QuotationDelivery;
+                    let QuotationRemark = obj.QuotationRemark;
+                    let EmployeeApproveId = obj.EmployeeApproveId;
+                    
+                    console.log(QuotationId)
+                    $.ajax({
+                        url: "/quotation_set/revise/" + QuotationId,
+                        method: 'post',
+                        contentType: 'application/json',
+                        data: JSON.stringify({
+                            QuotationNoId: QuotationNoId,
+                            QuotationRevised: QuotationRevised,
+                            QuotationStatus: QuotationStatus,
+                            QuotationSubject: QuotationSubject,
+                            QuotationTotalPrice: QuotationTotalPrice,
+                            QuotationDiscount: QuotationDiscount,
+                            QuotationValidityDate: QuotationValidityDate,
+                            QuotationPayTerm: QuotationPayTerm,
+                            QuotationDelivery: QuotationDelivery,
+                            QuotationRemark: QuotationRemark,
+                            EmployeeApproveId: EmployeeApproveId
+    
+                        }),
+                        success: function () {
+                            Swal.fire({
+                                position: 'center',
+                                icon: 'success',
+                                title: 'Created',
+                                text: 'Quotation data have been Edited',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                            tableQuo.ajax.reload(null, false);
+                        },
+                        error: function (err) {
+                            errorText = err.responseJSON.message;
+                            Swal.fire({
+                                position: 'center',
+                                icon: 'warning',
+                                title: 'Warning',
+                                text: errorText,
+                                showConfirmButton: true,
+                                confirmButtonText: 'OK',
+                                confirmButtonColor: '#FF5733'
+                            });
+                        }
+                    });
+			}
+        })
+        
+        
+
+               
     });
 
      //Delete
