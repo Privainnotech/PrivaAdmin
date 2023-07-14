77f264b2213b4ee82ef90b2dadd92d4945d375cb
Niwiar-Priva <87194989+Niwiar@users.noreply.github.com>
2023-07-14 09:56:28 +0700

    .

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const PORT = 2999;
 const dbconfig = {
   // main
-  // user: "privaadmin",
-  // password: "Jz2^7b8a",
-  // server: "119.59.96.61",
+  user: 'privaadmin',
+  password: 'Jz2^7b8a',
+  server: '119.59.96.61',
   // test
-  user: "sa",
-  password: "P@ssw0rd",
-  server: "192.168.1.6",
-  database: "PrivaDashboard",
+  // user: "sa",
+  // password: "P@ssw0rd",
+  // server: "192.168.1.6",
+  database: 'PrivaDashboard',
   port: 1433,
   options: {
     encrypt: false,
