2379c7a06d5f9e8f4f3771d347c36afaf62a1519
rew17p49 <106513736+rew17p49@users.noreply.github.com>
2023-08-21 16:51:10 +0700

    .

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const PORT = 2999;
 const dbconfig = {
   // main
-  user: 'privaadmin',
-  password: 'Jz2^7b8a',
-  server: '119.59.96.61',
+  // user: 'privaadmin',
+  // password: 'Jz2^7b8a',
+  // server: '119.59.96.61',
   // test
-  // user: "sa",
-  // password: "P@ssw0rd",
-  // server: "192.168.1.6",
+  user: "sa",
+  password: "P@ssw0rd",
+  server: "192.168.1.6",
   database: 'PrivaDashboard',
   port: 1433,
   options: {
